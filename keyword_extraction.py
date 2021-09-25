321ce8693f4d1433e3d748a3d495f89e761536be
GRajaraju <pgrajaraju@hotmail.com>
2021-09-25 11:04:59 +0530

    initial commit

---
+++ keyword_extraction.py
@@ -0,0 +1,10 @@
+"""
+The objective is to get keywords from a  given text.
+
+A naive approach would be, create a list of strong words that define the type of a document.
+For example: loan, amount, interest rate, date, lender, borrower, co-borrower are few words that indicates 
+the document is of type finance or mortage.
+
+
+
+"""
